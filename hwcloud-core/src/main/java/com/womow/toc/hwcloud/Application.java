154b125a36308c265dea60c34ee5fa52325b6d7a
常庆顺 <qshun@icloud.com>
2018-07-21 16:09:18 +0800

    init commit

---
+++ hwcloud-core/src/main/java/com/womow/toc/hwcloud/Application.java
@@ -0,0 +1,24 @@
+package com.womow.toc.hwcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication(scanBasePackages = {"com.womow"})
+@ImportResource({"classpath*:spring/**/spring-*.xml"})
+@EnableAspectJAutoProxy
+@EnableCaching
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+}
---
+++ hwcloud-rds/src/main/java/com/womow/toc/hwcloud/rds/RdsApplication.java
@@ -0,0 +1,11 @@
+package  com.womow.toc.hwcloud.rds;
+
+import com.womow.toc.hwcloud.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class RdsApplication {
+    public  static  void  main(String[] args){
+        Application.main(args);
+    }
+}
